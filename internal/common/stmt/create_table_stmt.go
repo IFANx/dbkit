ff99276e305a3cb5aa42274fcb4157260a85b28c
Jiansen Song <songjiansen20@otcaix.iscas.ac.cn>
2022-06-21 11:15:10 +0800

    update stmt

--- internal/common/stmt/create_table_stmt.go
+++ internal/common/stmt/create_table_stmt.go
@@ -1,1 +1,32 @@
 package stmt
+
+import (
+	"fmt"
+	"strings"
+)
+
+type CreateTableStmt struct {
+	TableName       string
+	Columns         []string
+	ColumnTypes     map[string]string
+	ColumnOptions   map[string]string
+	TableOptions    map[string]string
+	PartitionOption string
+}
+
+func (stmt *CreateTableStmt) String() string {
+	colDefs := make([]string, len(stmt.Columns))
+	for idx, colName := range stmt.Columns {
+		colDefs[idx] = colName + " " + stmt.ColumnTypes[colName]
+		if stmt.ColumnOptions[colName] != "" {
+			colDefs[idx] += " " + stmt.ColumnOptions[colName]
+		}
+	}
+	tabOps := make([]string, 0)
+	for key, val := range stmt.TableOptions {
+		tabOps = append(tabOps, key+"="+val)
+	}
+	sql := fmt.Sprintf("CREATE TABLE %s(%s) %s %s", stmt.TableName,
+		strings.Join(colDefs, ", "), strings.Join(tabOps, ", "), stmt.PartitionOption)
+	return sql
+}
--- internal/common/stmt/select_stmt.go
+++ internal/common/stmt/select_stmt.go
@@ -1,1 +1,30 @@
 package stmt
+
+import (
+	"fmt"
+	"strings"
+)
+
+type SelectStmt struct {
+	TableName string
+	Targets   []string
+	Predicate string
+	ForShare  bool
+	ForUpdate bool
+}
+
+func (stmt *SelectStmt) String() string {
+	sql := fmt.Sprintf("SELECT %s FROM %s WHERE %s",
+		strings.Join(stmt.Targets, ","), stmt.TableName, stmt.Predicate)
+	if stmt.ForShare {
+		sql += " FOR SHARE"
+	}
+	if stmt.ForUpdate {
+		sql += " FOR UPDATE"
+	}
+	return sql
+}
+
+func (stmt *SelectStmt) StringInMode() string {
+	return strings.Replace(stmt.String(), "FOR SHARE", "IN SHARE MODE", -1)
+}
