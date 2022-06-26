4736dddeefdaa033dd736ee9001002ed158bd85c
Jiansen Song <songjiansen20@otcaix.iscas.ac.cn>
2022-06-26 19:44:07 +0800

    add partial index and support multiple columns

--- internal/common/gen/create_index_gen.go
+++ internal/common/gen/create_index_gen.go
@@ -8,13 +8,30 @@ import (
 
 func GenerateCreateIndexStmt(indexName string, table *common.Table) *statement.CreateIndexStmt {
 
+	columns := make([]*common.Column, 0)
+	if randomly.RandIntGap(1, 10) == 1 { //multiple column
+		for _, val := range table.Columns {
+			if randomly.RandBool() {
+				columns = append(columns, val)
+			}
+		}
+	} else { // single column
+		for _, val := range table.Columns {
+			if randomly.RandBool() {
+				columns = append(columns, val)
+				break
+			}
+		}
+	}
+
 	return &statement.CreateIndexStmt{
 		OptionCreate:    randomly.RandIntGap(statement.CreOptFullText-1, statement.CreOptUnique),
 		IndexName:       indexName,
 		TypeIndex:       randomly.RandIntGap(statement.IndexBtree-1, statement.IndexHash),
 		TableName:       table.Name,
-		ColumnName:      randomly.RandPickOneStr(table.ColumnNames),
+		Columns:         columns,
 		OptionAlgorithm: randomly.RandIntGap(statement.AlgorCopy-1, statement.AlgorInplace),
 		OptionLock:      randomly.RandIntGap(statement.LockDefault, statement.LockShared),
+		Where:           GenerateExpr(columns, 3),
 	}
 }
--- internal/common/statement/create_index_stmt.go
+++ internal/common/statement/create_index_stmt.go
@@ -1,13 +1,19 @@
 package statement
 
+import (
+	"dbkit/internal/common"
+	"dbkit/internal/common/ast"
+)
+
 type CreateIndexStmt struct { //lack index_option
 	OptionCreate    CreateOption
 	IndexName       string
 	TypeIndex       IndexType
 	TableName       string
-	ColumnName      string //incomplete
+	Columns         []*common.Column
 	OptionAlgorithm AlgorithmOption
 	OptionLock      LockOption
+	Where           ast.AstNode
 }
 
 type CreateOption = int
@@ -56,7 +62,20 @@ func (stmt *CreateIndexStmt) String() string {
 	}
 	sql += "ON "
 	sql += stmt.TableName
-	sql += "(" + stmt.ColumnName + ")"
+	sql += "("
+
+	colCount := len(stmt.Columns) - 1
+	for idx, val := range stmt.Columns {
+		sql += val.Name
+		if val.Type.IsString() {
+			sql += "(10)" //TODO support random length
+		}
+		if idx != colCount {
+			sql += ", "
+		}
+	}
+
+	sql += ")"
 	if stmt.OptionAlgorithm != -1 {
 		algOptDic := []string{" COPY ", " DEFAULT ", " INPLACE "}
 		sql += " ALGORITHM =" + algOptDic[stmt.OptionAlgorithm]
@@ -64,6 +83,10 @@ func (stmt *CreateIndexStmt) String() string {
 	if stmt.OptionLock != -1 {
 		lockOptDic := []string{" DEFAULT ", " EXCLUSIVE ", " NONE ", " SHARED "}
 		sql += " LOCK =" + lockOptDic[stmt.OptionLock]
+	}
+
+	if stmt.Where != nil {
+		sql += "WHERE " + stmt.Where.String() + " "
 	}
 
 	return sql
