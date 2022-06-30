dd1b3835eb2a952c90263c08527a4535aa2eb501
Qianwang Dai <dddinary@163.com>
2022-06-30 23:37:24 +0800

    完善任务终止逻辑

--- internal/model/test_job.go
+++ internal/model/test_job.go
@@ -99,3 +99,18 @@ func AbortTestJob(jid int) error {
 	}
 	return nil
 }
+
+func EndTestJob(jid int, success bool) error {
+	state := 2
+	if !success {
+		state = -1
+	}
+	sql := fmt.Sprintf("UPDATE %s SET state = %d WHERE jid = %d", tableNameTestJob, state, jid)
+	_, err := db.Exec(sql)
+	if err != nil {
+		errMsg := fmt.Sprintf("更新TestJob状态失败：%s\n", err)
+		log.Warnf(errMsg)
+		return errors.New(errMsg)
+	}
+	return nil
+}
